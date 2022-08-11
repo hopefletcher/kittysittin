f786d16650bdeb2be842ef7d21f773da861fca26
Hope Fletcher <thehopefletcher@gmail.com>
2022-08-11 20:27:51 +0200

    created the routes for cats, bookings, my cats and my bookings

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cats do
+    resources :bookings, only: [ :index, :show, :new, :create, :destroy ]
+  end
+  get "my-bookings", to: "bookings#my_bookings"
+  get "my-cats", to: "cats#my_cats"
 end
