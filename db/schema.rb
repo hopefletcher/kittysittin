fe1c8ed9338de47f6583d76f1f68c3983b83f3c2
Hope Fletcher <thehopefletcher@gmail.com>
2022-08-11 19:33:16 +0200

    Migrated booking table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_11_173105) do
+ActiveRecord::Schema.define(version: 2022_08_11_173158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "cat_id", null: false
+    t.bigint "user_id", null: false
+    t.string "location"
+    t.date "start_date"
+    t.date "end_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cat_id"], name: "index_bookings_on_cat_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "cats", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -39,5 +51,7 @@ ActiveRecord::Schema.define(version: 2022_08_11_173105) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "cats"
+  add_foreign_key "bookings", "users"
   add_foreign_key "cats", "users"
 end
