da0e6ecb1f2c002cfb55b7cbb1f4beff43b0d2e1
Hope Fletcher <thehopefletcher@gmail.com>
2022-08-14 16:32:51 +0200

    added crud actions to cats

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,2 +1,50 @@
 class CatsController < ApplicationController
+  before_action :set_cat, only: [:show, :edit, :update, :dstroy]
+
+  def index
+    @cats = Cat.all
+  end
+
+  def show; end
+
+  def new
+    @cat = Cat.new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+    @cat.save
+    @cat.user = current_user
+    if @cat.save
+      redirect_to cat_path(cat)
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    @cat.update(cat_params)
+    redirect_to cat_path(@cat)
+  end
+
+  def destroy
+    @cat.destroy
+    redirect_to cat_path
+  end
+
+  def my_cats
+    @cats = Cat.where(user: current_user)
+  end
+
+  private
+
+  def set_cat
+    @cat = Cat.find(paramss[:id])
+  end
+
+  def cat_params
+    params.require(:cat).permit(:location, :age, :description, :house_cat, :pay)
+  end
 end
